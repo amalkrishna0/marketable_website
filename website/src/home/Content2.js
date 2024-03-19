f3318f2a0f204ef833ea62506599f4e4993d07e2
amalkrishna0 <amalkrishna528@gmail.com>
2024-03-19 14:48:32 +0530

    Created 5 point boxes

--- website/src/home/Content2.js
+++ website/src/home/Content2.js
@@ -16,35 +16,93 @@ export default function Content2() {
         <div className="heading2_content2">MEET THE TEAM</div>
 
         <div className="container2">
-            <div className="card">
-                <div className="img1"></div>
-                <div className="intro">
-                    <p>CEO AND FOUNDER</p>
-                    <h1>BINOY BABU</h1>
-                </div>
+          <div className="card">
+            <div className="img1"></div>
+            <div className="intro">
+              <p>CEO AND FOUNDER</p>
+              <h1>BINOY BABU</h1>
             </div>
-            <div className="card">
+          </div>
+          <div className="card">
             <div className="img2"></div>
-                <div className="intro">
-                    <p>CTO AND SMC TRADER</p>
-                    <h1>SREEKANTH K</h1>
-
-                </div>
+            <div className="intro">
+              <p>CTO AND SMC TRADER</p>
+              <h1>SREEKANTH K</h1>
             </div>
-            <div className="card">
+          </div>
+          <div className="card">
             <div className="img3"></div>
-                <div className="intro">
-                    <p>BLOCKCHAIN EXPERT AND WEB3 DEVELOPER</p>
-                    <h1>SANDYAGU R</h1>
-                </div>
+            <div className="intro">
+              <p>BLOCKCHAIN EXPERT AND WEB3 DEVELOPER</p>
+              <h1>SANDYAGU R</h1>
             </div>
-            <div className="card">
-              <div className="img4"></div>
-                <div className="intro">
-                    <p>PUBLICITY DESIGNER AND MEDIA HEAD</p>
-                    <h1>ANUGRAH</h1>
-                </div>
+          </div>
+          <div className="card">
+            <div className="img4"></div>
+            <div className="intro">
+              <p>PUBLICITY DESIGNER AND MEDIA HEAD</p>
+              <h1>ANUGRAH</h1>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="buy_course">
+        <div className="for_who">WHY SHOULD YOU BUY THIS COURSE?</div>
+        <div className="for_who_desc">
+          <div className="first_desc">
+            <div className="img_desc1"></div>
+            <div className="desc1">
+              <h1>Learn from Scratch:</h1>
+              <p>
+                Perfect for beginners, this course starts with the basics,
+                making it easy to understand even if you're entirely new to
+                crypto trading.
+              </p>
+            </div>
+          </div>
+          <div className="first_desc">
+            <div className="img_desc1"></div>
+            <div className="desc1">
+              <h1>Expert Guidance:</h1>
+              <p>
+                You'll benefit from experts' knowledge, simplifying complex
+                topics and guiding you through every step in a straightforward
+                manner.
+              </p>
+            </div>
+          </div>
+          <div className="first_desc">
+            <div className="img_desc1"></div>
+            <div className="desc1">
+              <h1>Practical Learning:</h1>
+              <p>
+                Dive into real-life examples and exercises, gaining hands-on
+                experience that builds your confidence for actual trading.
+              </p>
+            </div>
+          </div>
+          <div className="first_desc">
+            <div className="img_desc1"></div>
+            <div className="desc1">
+              <h1>Stay Safe:</h1>
+              <p>
+                Discover essential risk management tips, ensuring you can make
+                informed decisions while navigating the ups and downs of crypto
+                markets.
+              </p>
+            </div>
+          </div>
+          <div className="first_desc">
+            <div className="img_desc1"></div>
+            <div className="desc1">
+              <h1>Premium Community Access:</h1>
+              <p>
+                Lifetime free access to our exclusive members only Discord
+                Community section and Telegram channel were our Master traders
+                post their Technical Analysis.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
